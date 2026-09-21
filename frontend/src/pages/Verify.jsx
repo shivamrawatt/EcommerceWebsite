6650e497bfcb6d18e3b4dce35b0b0f0f18dc59d9
Shivam Rawat <tulyarthdigiweb68@gmail.com>
2026-09-21 15:08:24 +0530

    openship 5

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -4,6 +4,8 @@ import { useSearchParams } from 'react-router-dom'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
+
+
 const Verify = () => {
     const {navigate,token,setCartItems,backendUrl} = useContext(ShopContext)
     const [searchParams,setSearchParams]=useSearchParams()
